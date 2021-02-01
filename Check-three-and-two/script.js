67834002832f74e7d552c1ef1dd900e914120350
Ida <so.kida@hotmail.com>
2021-01-31 22:05:21 -0500

    new cata

---
+++ Check-three-and-two/script.js
@@ -0,0 +1,25 @@
+function checkThreeAndTwo(array) {
+    let filterA = array.filter(elem => elem === 'a').length;
+   let filterB = array.filter(elem => elem === 'b').length;
+   let filterC = array.filter(elem => elem === 'c').length;
+   
+   return (filterA === 3 || filterB === 3 || filterC === 3) && (filterA === 2 || filterB === 2 || filterC === 2);
+   }
+
+
+   function checkThreeAndTwo(array) {
+    const counts = array.reduce((result, letter) => {
+      if (result[letter]) {
+        result[letter]++;
+      } else {
+        result[letter] = 1;
+      }
+      
+      return result;
+    }, {});
+    
+    const containsThree = Object.values(counts).some(x => x === 3);
+    const containsTwo = Object.values(counts).some(x => x === 2);
+    
+    return containsThree && containsTwo;
+  }
---
+++ maximum-product/readme.md
@@ -0,0 +1,11 @@
+https://www.codewars.com/kata/5a4138acf28b82aa43000117/train/javascript
+
+Task
+
+Given an array of integers , Find the maximum product obtained from multiplying 2 adjacent numbers in the array.
+
+Notes
+
+Array/list size is at least 2.
+
+Array/list numbers could be a mixture of positives, negatives also zeroes .
