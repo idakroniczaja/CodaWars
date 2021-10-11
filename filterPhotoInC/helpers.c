0802cd37e80a80623c18787818d2add019cf91cf
idakroniczaja <so.kida@hotmail.com>
2021-10-11 17:05:03 -0400

    blur filter for photo in c

--- filterPhotoInC/helpers.c
+++ filterPhotoInC/helpers.c
@@ -42,6 +42,98 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE newImage[height][width];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            newImage[i][j] = image[i][j];
+        }
+    }
+
+    for (int i = 0, red, green, blue, counter; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            red = green = blue = counter = 0;
+            // add the center pixel
+            if (i >= 0 && j >= 0)
+            {
+                red += newImage[i][j].rgbtRed;
+                green += newImage[i][j].rgbtGreen;
+                blue += newImage[i][j].rgbtBlue;
+                counter++;
+            }
+            // add below pixel
+            if (i >= 0 && j - 1 >= 0)
+            {
+                red += newImage[i][j-1].rgbtRed;
+                green += newImage[i][j-1].rgbtGreen;
+                blue += newImage[i][j-1].rgbtBlue;
+                counter++;
+            }
+            // add right pixel
+            if ((i >= 0 && j + 1 >= 0) && (i >= 0 && j + 1 < width))
+            {
+                red += newImage[i][j+1].rgbtRed;
+                green += newImage[i][j+1].rgbtGreen;
+                blue += newImage[i][j+1].rgbtBlue;
+                counter++;
+            }
+            // add left pixel
+            if (i - 1 >= 0 && j >= 0)
+            {
+                red += newImage[i-1][j].rgbtRed;
+                green += newImage[i-1][j].rgbtGreen;
+                blue += newImage[i-1][j].rgbtBlue;
+                counter++;
+            }
+            // add left below pixel
+            if (i - 1 >= 0 && j - 1 >= 0)
+            {
+                red += newImage[i-1][j-1].rgbtRed;
+                green += newImage[i-1][j-1].rgbtGreen;
+                blue += newImage[i-1][j-1].rgbtBlue;
+                counter++;
+            }
+            // add left upper pixel
+            if ((i - 1 >= 0 && j + 1 >= 0) && (i - 1 >= 0 && j + 1 < width))
+            {
+                red += newImage[i-1][j+1].rgbtRed;
+                green += newImage[i-1][j+1].rgbtGreen;
+                blue += newImage[i-1][j+1].rgbtBlue;
+                counter++;
+            }
+            // add upper pixel
+            if ((i + 1 >= 0 && j >= 0) && (i + 1 < height && j >= 0))
+            {
+                red += newImage[i+1][j].rgbtRed;
+                green += newImage[i+1][j].rgbtGreen;
+                blue += newImage[i+1][j].rgbtBlue;
+                counter++;
+            }
+            // add below right pixel
+            if ((i + 1 >= 0 && j - 1 >= 0) && (i + 1 < height && j - 1 >= 0))
+            {
+                red += newImage[i+1][j-1].rgbtRed;
+                green += newImage[i+1][j-1].rgbtGreen;
+                blue += newImage[i+1][j-1].rgbtBlue;
+                counter++;
+            }
+            // add upper right pixel
+            if ((i + 1 >= 0 && j + 1 >= 0) && (i + 1 < height && j + 1 < width))
+            {
+                red += newImage[i+1][j+1].rgbtRed;
+                green += newImage[i+1][j+1].rgbtGreen;
+                blue += newImage[i+1][j+1].rgbtBlue;
+                counter++;
+            }
+
+            image[i][j].rgbtRed = round(red / (counter * 1.0));
+            image[i][j].rgbtGreen = round(green / (counter * 1.0));
+            image[i][j].rgbtBlue = round(blue / (counter * 1.0));
+        }
+    }
 
     return;
 }
