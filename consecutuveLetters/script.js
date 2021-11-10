21310eb723fff8875a9e96ea294fbaba457a12fb
idakroniczaja <so.kida@hotmail.com>
2021-11-09 23:04:33 -0500

    kata done

---
+++ consecutuveLetters/script.js
@@ -0,0 +1,30 @@
+// In this Kata, we will check if a string contains consecutive letters as they appear in the English alphabet and if each letter occurs only once.
+
+// Rules are: (1) the letters are adjacent in the English alphabet, and (2) each letter occurs only once.
+
+// For example: 
+// solve("abc") = True, because it contains a,b,c
+// solve("abd") = False, because a, b, d are not consecutive/adjacent in the alphabet, and c is missing.
+// solve("dabc") = True, because it contains a, b, c, d
+// solve("abbc") = False, because b does not occur once.
+// solve("v") = True
+// All inputs will be lowercase letters.
+
+// More examples in test cases. Good luck!
+
+
+function solve(s) {
+    return 'abcdefghijklmnopqrstuvwxyz'.includes([...s].sort().join(''));
+  }
+
+
+//SOLUTION2
+
+
+function solve(s){
+  s=s.split("").sort();
+  for (var i=1; i<s.length; ++i)
+    if (s[i-1].charCodeAt()!=s[i].charCodeAt()-1)
+      return false
+  return true
+}
