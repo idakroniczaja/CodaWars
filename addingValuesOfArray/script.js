0ea12f87adcb7d960dcd05f189fce0fecfd5eeb7
idakroniczaja <so.kida@hotmail.com>
2022-01-31 22:27:32 -0500

    fixed mistake

--- addingValuesOfArray/script.js
+++ addingValuesOfArray/script.js
@@ -10,22 +10,7 @@ The shifting value is always a value from 0 up to the size of the arrays.
 There are always arrays in the array, so you do not need to check for null or empty.
 #1. Example:
 [[1,2,3,4,5,6], [7,7,7,7,7,-7]], 0
-1,2,3,4,5,6
-7,7,7,7,7,-7
---> [8,9,10,11,12,-1]
-#2. Example
-[[1,2,3,4,5,6], [7,7,7,7,7,7]], 3
-1,2,3,4,5,6
-      7,7,7,7,7,7
---> [1,2,3,11,12,13,7,7,7]
-#3. Example
-[[1,2,3,4,5,6], [7,7,7,-7,7,7], [1,1,1,1,1,1]], 3
-1,2,3,4,5,6
-      7,7,7,-7,7,7
-            1,1,1,1,1,1
---> [1,2,3,11,12,13,-6,8,8,1,1,1]
-Have fun coding it and please don't forget to vote and rank this kata! :-)
-I have created other katas. Have a look if you like coding and challenges.
+
 */
 function addingShifted (arrayOfArrays, shift) {    
     var result = [];
